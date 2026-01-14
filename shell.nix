71ecb7e8c3251bab68cea1f238436947e1de1617
Filip Pękala <272985@student.pwr.edu.pl>
2026-01-14 20:25:07 +0100

    Make bash interactive after `nix develop`
    There is probably some better fix for this. It feels dumb.

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
 {everybody-codes, ...}: {pkgs, ...}:
 pkgs.mkShell {
+  buildInputs = [pkgs.bashInteractive];
+
+  shellHook = ''
+    export SHELL="${pkgs.bashInteractive}/bin/bash";
+  '';
   CPATH = "${everybody-codes}/include";
   LIBRARY_PATH = "${everybody-codes}/lib";
 }
